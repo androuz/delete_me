eeda24841144b1875ea7607eb70cfd2cb6dc99be
androuz <sarnitoabeniacar@hotmail.com>
2024-03-18 11:35:22 +0100

    S3 presigned urls

--- src/main/java/com/example/batchprocessing/S3Config.java
+++ src/main/java/com/example/batchprocessing/S3Config.java
@@ -6,7 +6,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -29,12 +29,23 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey);
-        System.out.println("Configured AWS Region: " + URI.create(customEndpointUrl)); // This should now print the correct value
+        System.out.println("Configured AWS Region: " + awsRegion); // This should now print the correct value
 
         return S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .endpointOverride(URI.create(customEndpointUrl))
                 .region(Region.of(awsRegion))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey);
+
+        return S3Presigner.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .region(Region.of(awsRegion))
+                .endpointOverride(URI.create(customEndpointUrl))
                 .build();
     }
 }
---
+++ src/main/java/com/example/batchprocessing/service/S3Service.java
@@ -0,0 +1,44 @@
+package com.example.batchprocessing.service;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+import java.util.Map;
+
+import java.time.Duration;
+import org.slf4j.Logger;
+
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+
+    public S3Service(S3Client s3Client, S3Presigner s3Presigner) {
+        this.s3Client = s3Client;
+        this.s3Presigner = s3Presigner;
+    }
+
+    /* Create a presigned URL to use in a subsequent PUT request */
+    public String createPresignedUrl(String bucketName, String keyName, Map<String, String> metadata) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .metadata(metadata)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                .putObjectRequest(objectRequest)
+                .build();
+
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+        String myURL = presignedRequest.url().toString();
+        System.out.println("Presigned URL to upload a file to: " + myURL);
+        System.out.println("HTTP method: " + presignedRequest.httpRequest().method());
+
+        return presignedRequest.url().toExternalForm();
+    }
+}
